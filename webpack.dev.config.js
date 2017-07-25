0dc694df0d406ab02d30a0fb75c0d23a01fe0151
Sean Johnson <sjohnson@sjohnsonaz.com>
2017-07-25 16:14:04 -0700

    Updated npm scripts.

---
+++ webpack.dev.config.js
@@ -0,0 +1,42 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: {
+        'main': './src/styl/main.styl',
+        'index': './test/ts/index.ts'
+    },
+    output: {
+        filename: './test/build/[name].js',
+    },
+    resolve: {
+        extensions: ['.webpack.js', '.web.js', '.js', '.css', '.styl']
+    },
+    module: {
+        loaders: [{
+            test: /\.styl$/,
+            loader: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: [
+                    'css-loader',
+                    { loader: 'postcss-loader', options: { sourceMap: true } },
+                    'stylus-loader'
+                ]
+            })
+        }, {
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: [
+                    'css-loader',
+                    { loader: 'postcss-loader', options: { sourceMap: true } }
+                ]
+            })
+        }, {
+            test: /\.tsx?$/,
+            loader: 'ts-loader'
+        }]
+    },
+    plugins: [
+        new ExtractTextPlugin("./dist/[name].css")
+    ]
+};
