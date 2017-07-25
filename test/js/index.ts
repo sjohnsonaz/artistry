e123443377f74dfc65fa8b5ddbf9d5d62ed2dd7f
Sean Johnson <sjohnson@sjohnsonaz.com>
2017-07-25 13:16:08 -0700

    Fixed merge again.

--- test/js/index.ts
+++ test/js/index.ts
@@ -143,7 +143,7 @@ $(function () {
         var ul = $($(element).children()[0]);
         var div = $($(element).children()[1]);
         ul.children().each(function (index, element) {
-            $(element).first('a').click(function (event) {
+            $(element).find('a').click(function (event) {
                 ul.children().removeClass('tab-active');
                 div.children().removeClass('tab-active');
                 $(element).addClass('tab-active');
