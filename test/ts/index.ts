d3629f628f40c28a4a65d4d0b4310e6f8ff15cfe
Sean Johnson <sjohnson@sjohnsonaz.com>
2017-08-12 00:58:48 -0700

    Fixed scroll locking in Firefox.

--- test/ts/index.ts
+++ test/ts/index.ts
@@ -40,13 +40,14 @@ $(function () {
         let body = document.body;
         let root = document.querySelector('.root');
         if (lock) {
-            let scrollTop = body.scrollTop;
+            let scrollTop = window.pageYOffset || document.documentElement.scrollTop || body.scrollTop || root.scrollTop;
             body.classList.add('body-scroll-lock');
             root.scrollTop = scrollTop;
         } else {
-            let scrollTop = root.scrollTop;
+            let scrollTop = root.scrollTop || window.pageYOffset || document.documentElement.scrollTop || body.scrollTop;
             body.classList.remove('body-scroll-lock');
             body.scrollTop = scrollTop;
+            document.documentElement.scrollTop = scrollTop;
         }
     }
 
