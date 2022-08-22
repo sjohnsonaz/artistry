2a4272d9f0860d6dce02f0d63cd4bb064ffc39ac
Sean Johnson <sean.johnson.arizona@gmail.com>
2022-08-22 13:55:25 -0700

    Created example build.

---
+++ webpack.example.js
@@ -0,0 +1,83 @@
+const path = require('path');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+const mode = process.env.NODE_ENV || 'development';
+
+module.exports = {
+    mode,
+    entry: {
+        'artistry': './src/styl/main.styl',
+        'main': './src/examples/scripts/main.ts'
+    },
+    output: {
+        filename: '[name]-[fullhash].js',
+        path: path.resolve(__dirname, 'build'),
+    },
+    devtool: 'inline-source-map',
+    resolve: {
+        extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js', '.css', '.styl'],
+        plugins: [
+            new TsconfigPathsPlugin({
+                configFile: './tsconfig.json',
+            }),
+        ],
+    },
+
+    module: {
+        rules: [{
+            test: /\.styl$/,
+            use: [
+                'style-loader',
+                'css-loader',
+                { loader: 'postcss-loader', options: { sourceMap: true } },
+                'stylus-loader'
+            ]
+        }, {
+            test: /\.css$/,
+            use: [
+                'style-loader',
+                'css-loader',
+                { loader: 'postcss-loader', options: { sourceMap: true } }
+            ]
+        }, {
+            test: /\.hbs$/,
+            use: ['handlebars-loader']
+        }, {
+            test: /\.tsx?$/,
+            use: [{
+                loader: 'ts-loader',
+                options: {
+                    transpileOnly: true,
+                },
+            }],
+        }, {
+            test: /\.(woff|woff2|eot|ttf|svg)$/,
+            use: ['file-loader'],
+        }]
+    },
+    plugins: [
+        new ForkTsCheckerWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/examples/html/index.html',
+        }),
+    ],
+    optimization: {
+        runtimeChunk: 'single',
+    },
+    devServer: {
+        // host: '0.0.0.0',
+        // static: {
+        //     directory: path.join(__dirname, 'www'),
+        // },
+        // compress: true,
+        port: 8090,
+        // historyApiFallback: true,
+        // disableHostCheck: true,
+    },
+};
